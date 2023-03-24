a1f29a234ab94b89c986b24e08dcfb7a450f87cd
Alex L <95913221+Pwhsky@users.noreply.github.com>
2023-03-24 17:53:52 +0100

    Add files via upload

--- ESN_lorenz.py
+++ ESN_lorenz.py
@@ -1,41 +1,83 @@
 import numpy as np
 import reservoirpy
+import scipy as sci
 from  matplotlib import pyplot as plt
 from reservoirpy.datasets import lorenz
 from reservoirpy.nodes import Reservoir, Ridge, Input
+data = np.load("xyz_coordinates.npy")
+data = np.transpose(data)
 
-#For reproducability:
-reservoirpy.set_seed(42)
+#Parameters:
+neurons=205
+M = 3
+p = 0.95
+
+X = data[0:int(len(data)*p),:] #training data
+Y = data[int(len(data)*p):-1,:] #Validation data
 
 
-fig = plt.figure()
-trainSize = 95000
+def generateWeights():
+    w_res = np.random.randn(neurons,neurons)*(2/neurons)
+    w_in  = np.random.randn  (neurons,M)*(1/neurons)
+    return w_res,w_in
 
-X = np.load("xyz_coordinates.npy")
-X = np.transpose(X)
-t = np.arange(len(X)-trainSize)
 
-plt.title("Test data")
-plt.plot(t,X[trainSize:,1])
-plt.ylabel("y(t)")
-plt.xlabel("t")
+W_res,W_input = generateWeights()
 
-time2 = np.arange(4999)
+#Reservoir neuron states (r)
+states              = np.zeros((1,neurons))
+states_over_time    = np.zeros((len(X),neurons))
+states_over_time[0,:] = states
 
-reservoir1 = Reservoir(1000, lr=0.3, sr=1.1)
-reservoir2 = Reservoir(1000, lr=0.3, sr=1.1)
-readout = Ridge(ridge=1e-6)
-esn = reservoir1 >> reservoir2>> readout
-esn.fit(X[:trainSize-1],X[1:trainSize],warmup=100)
-predictions = esn.run(X[trainSize:-1])
+#append new states with: states_over_time[t,:] = states
 
-plt.plot(time2,predictions[:,1],r'-', linewidth=1,)
-plt.ylim((-25,25))
+
+for t in range(len(X)-1):
+    
+    #compute new states according to (1a):
+    recurrent_term = np.squeeze(np.dot(W_res,np.transpose(states)))
+    current_term = np.dot(W_input,X[t,:])
+    new_states = np.tanh(np.transpose(np.add(recurrent_term,current_term)))
+    
+    #add the new states to our storage
+    states_over_time[t+1,:] = new_states
+    
+    #update so that the new states can be used in the next iteration
+    states = new_states
+
+#Apply ridge regression to train the output matrix
+kI = np.eye(neurons)*0.005
+R = states_over_time
+term1 = np.linalg.inv((np.dot(np.transpose(R),R)+kI))
+term2 = np.dot(np.transpose(R),X)
+W_out =  np.dot(term1,term2)
+
+#predict the future:
+O = np.dot(states,W_out)
+predicted_coordinates = np.zeros((len(Y),3))
+
+for i in range(len(predicted_coordinates)):
+    O = np.dot(states,W_out)
+    recurrent_term = np.squeeze(np.dot(W_res,np.transpose(states)))
+    current_term = np.dot(W_input,O[:])
+    new_states = np.tanh(np.transpose(np.add(recurrent_term,current_term)))
+    states = new_states;
+    predicted_coordinates[i,:] = O
+
+
+x = predicted_coordinates[:,0]
+y = predicted_coordinates[:,1]
+z = predicted_coordinates[:,2]
 
 fig2 = plt.figure()
 
-difference = np.abs(predictions[:,1]-X[95001:,1])
+
+plt.plot(np.arange(len(Y)),Y[:,1],label = "Ground Truth")
+plt.plot(np.arange(len(Y)),y,label = "Predicted")
+plt.legend()
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection = '3d')
+#plt.plot(x,y,z,linewidth = 0.6)
 
 
-plt.plot(time2,difference,linewidth=0.5)
-plt.title("test vs prediction difference")
+
--- lorenz.py
+++ lorenz.py
@@ -1,34 +1,35 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import linspace
+from scipy.integrate import solve_ivp
 #This script will generate coordinates over time
 #for the Lorenz attractor
+
 s = 10  #sigma
 r = 28  #Rho
 b = 8/3 #beta
 
-time_steps = 100000 #simulation length
-dt = 0.008         #resolution of trajectory
+duration       = 200 #how long to simulate for.
+resolution     = 50000
+start_position = [1,1,1]
 
-#Initialize space
-x = np.zeros((time_steps))
-y = np.zeros((time_steps))
-z = np.zeros((time_steps))
-
-#initial values must be non-zero
-x[0] = 1
-y[0] = 1
-z[0] = -2
-
-#Run the dynamics:
-for t in range(time_steps-1):
-    x[t+1] = x[t] + ( s*(y[t] - x[t])       )*dt
-    y[t+1] = y[t] + ( x[t]*(r - z[t]) - y[t])*dt
-    z[t+1] = z[t] + ( x[t]*y[t] - b*z[t]   )*dt
+t = np.linspace(0,duration,resolution) #time 
 
 
+def lorenz_system(t,xyz):
+    x,y,z = xyz
+    return [ s*(y - x),
+             r*x - x*z - y, 
+             x*y - b*z ]
 
+trajectory = solve_ivp(lorenz_system, [0,duration],start_position,t_eval=t)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
-plt.plot(x,y,z,linewidth = 1)
-plt.savefig("lorenz.pdf")
-np.save("xyz_coordinates",(x,y,z))
+plt.plot(trajectory.y[0],trajectory.y[1],trajectory.y[2],linewidth = 0.6)
+np.save("xyz_coordinates",(trajectory.y[0],
+                           trajectory.y[1],
+                           trajectory.y[2]))
+
+
+
+    
