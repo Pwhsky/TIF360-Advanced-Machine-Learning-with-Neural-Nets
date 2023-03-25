3cfbadb758cf741c2a1d679ce57eae93e8de0064
Alex L <95913221+Pwhsky@users.noreply.github.com>
2023-03-25 13:01:13 +0100

    Add files via upload

--- lorenz.py
+++ lorenz.py
@@ -11,8 +11,8 @@ r = 28  #Rho
 b = 8/3 #beta
 
 
-duration       = 40             #how long to simulate for. #40 for producing test data
-resolution     = 1000000        #100000 for training data
+duration       = 80           #how long to simulate for. #40 for producing test data
+resolution     = 200000        #100000 for training data
 start_position1 = [1,1,1]
 separation = 1e-9
 
@@ -27,21 +27,23 @@ def lorenz_system(t,xyz):
              r*x - x*z - y, 
              x*y - b*z ]
 
+#Generate a trajectory for the lorenz attractor:
 trajectory1 = solve_ivp(lorenz_system, [0,duration],start_position1,t_eval=t)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 plt.plot(trajectory1.y[0],trajectory1.y[1],trajectory1.y[2],linewidth = 0.6)
-#np.save("xyz_coordinates",(trajectory1.y[0],
-#                           trajectory1.y[1],
-#                           trajectory1.y[2]))
+np.save("xyz_coordinates",(trajectory1.y[0],
+                          trajectory1.y[1],
+                          trajectory1.y[2]))
 
 #To solve for lyaponov time, calculate a second trajectory 
 #slightly offset and compute euclidian distance
 
-#Compute lyaponov exponent:
+#Compute a second trajectory slightly offset:
 trajectory2 = solve_ivp(lorenz_system, [0,duration],start_position2,t_eval=t)
 
+#compute euclidean distance between the trajectories
 distance = np.sqrt((np.abs(trajectory1.y[0]-trajectory2.y[0]))**2 +
                    (np.abs(trajectory1.y[1]-trajectory2.y[1]))**2 +
                    (np.abs(trajectory1.y[2]-trajectory2.y[2]))**2 )
