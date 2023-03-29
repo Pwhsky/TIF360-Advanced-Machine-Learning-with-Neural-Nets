b2d23384f6c2368c540846ead74d7a3bf06e7cca
Alex L <95913221+Pwhsky@users.noreply.github.com>
2023-03-29 18:32:25 +0200

    Add files via upload

--- GNN.py
+++ GNN.py
@@ -1,2 +1,117 @@
-#Graph Neural Nets Homework A
-from torch_geometric.datasets import Planetoid
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch_geometric
+import networkx as nx
+from torch_geometric.loader import DataLoader
+import torch_geometric.utils as utils
+import torch.nn.functional as F
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool, GraphConv, GATConv, GCNConv
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+
+
+dataset = torch.load('data/graph_data_decoding_2023.pt')
+
+#%%
+
+print('====================')
+print(f'Number of graphs: {len(dataset)}')
+print('=============================================================')
+data = dataset[20]  # Get one data point.
+
+print('Some properties of a graph in the dataset:')
+print()
+
+print(data)
+# Gather some statistics about the graph.
+print(f'Number of nodes: {data.num_nodes}')
+print(f'Number of edges: {data.num_edges}')
+print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+print(f'Has self-loops: {data.has_self_loops()}')
+print(f'Is undirected: {data.is_undirected()}')
+print(f'Number of node features: {data.num_node_features}')
+
+label=data.y
+label_X=label[:,0]
+label_Z=label[:,1]
+print(label_X,label_Z)
+#The data point dataset[20] is thus in class X
+
+
+edge_index = data.edge_index.t()
+edge_index=edge_index.numpy()
+feature=data.x.numpy()
+edge=data.edge_attr.numpy()
+
+graph = dataset[20]
+g = utils.to_networkx(graph, to_undirected=True)
+positions = {}
+label_positions = {}
+node_labels = {}
+edge_labels={}
+x = graph.x
+
+for i in range(x.shape[0]):
+    positions[i] = np.array([x[i, 2], x[i, 3]])
+    label_positions[i] = np.array([x[i, 2], x[i, 3]])
+    node_labels[i] = np.array([x[i, 0], x[i, 1], x[i, 2], x[i, 3]])
+for i in range(edge_index.shape[0]):
+    edge_labels[(edge_index[i,0],edge_index[i,1])]=edge[i]
+    
+fig, ax = plt.subplots(figsize = (10, 10))
+nx.draw_networkx(g, ax=ax, pos = positions, labels = node_labels, with_labels = False)
+nx.draw_networkx_labels(g, ax=ax, pos = label_positions, labels = node_labels, verticalalignment= 'top')
+nx.draw_networkx_edge_labels(g,ax=ax,  pos = positions,edge_labels=edge_labels)
+ax.axis('off')
+
+# split the dataset into test and validation:
+num_samples = len(dataset)
+train_data = dataset[int(.05 * num_samples ) :] # 95% train
+test_data = dataset[: int(.05 * num_samples )] # 5% test
+train_loader = DataLoader(train_data, batch_size=1000, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=1000, shuffle=True)
+#%%
+
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        torch.manual_seed(1234567)
+        self.conv1 = GCNConv(data.num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, 4)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+model = GCN(hidden_channels=16)
+
+#%%
+
+#Visualize data:
+
+    
+def visualize(h, color):
+    z = TSNE(n_components=2,perplexity = 4).fit_transform(h.detach().cpu().numpy())
+
+    plt.figure(figsize=(10,10))
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.scatter(z[:, 0], z[:, 1], s=70, c=color, cmap="Set2")
+    plt.show()
+
+
+model = GCN(hidden_channels=16)
+model.eval()
+out = model(data.x, data.edge_index)
+visualize(out, color=['red','blue','green','orange','yellow'])
+
